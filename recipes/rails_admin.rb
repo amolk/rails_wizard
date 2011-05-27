fa7a406153ec9d70f63892b46659b51d2970168d
Amol Kelkar <amolk@aflatune.com>
2011-05-27 01:52:21 -0700

    Fixed generate and rake commands

--- recipes/rails_admin.rb
+++ recipes/rails_admin.rb
@@ -1,9 +1,9 @@
 gem 'rails_admin', :git => 'git://github.com/sferik/rails_admin.git'
 
 after_bundler do
-  generate 'rails_admin:install_admin'
-  rake 'admin:copy_assets'
-  rake 'admin:ckeditor_download' if config['ckeditor']
+  generate 'rails_admin'
+  rake 'rails_admin:copy_assets'
+  rake 'rails_admin:ckeditor_download' if config['ckeditor']
 end
 
 __END__
