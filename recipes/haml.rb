3fb8d460befe727ee60364a22a53c25fadbedc4f
Amol Kelkar <amolk@aflatune.com>
2011-05-27 01:53:09 -0700

    Add dependencies for HAML

--- recipes/haml.rb
+++ recipes/haml.rb
@@ -1,6 +1,9 @@
 gem 'haml', '>= 3.0.0'
 gem 'haml-rails'
 
+# hpricot and ruby_parser required by haml
+gem "hpricot", :group => :development
+gem "ruby_parser", :group => :development
 __END__
 
 name: HAML
